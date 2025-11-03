c5d1c569a33ee3d89b74bd8add57c44629ecc054
dxviiix <159883071+dxviiix@users.noreply.github.com>
2025-11-03 19:37:20 +0100

    trabajo

---
+++ Suma.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Calcula la suma de dos números.
+ *
+ * @param int $a Primer número.
+ * @param int $b Segundo número.
+ * @return int Suma de a y b.
+ */
+function sumar($a, $b) {
+    return $a + $b;
+}
+?>
